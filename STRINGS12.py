b88ca220b44903c09d2476b07b6f19062b914148
abhijitramesh <43090559+abhijitramesh@users.noreply.github.com>
2018-10-17 20:20:38 +0530

    Update on string
    commented and did some minor bug fixes

--- STRINGS12.py
+++ STRINGS12.py
@@ -1,16 +1,16 @@
-list= [""] * 12
-for i in range (12):
-    str1=raw_input("enter name:")
-    list[i]=str1
-i=0
-for i in range (10):
-    strr=list[i]+list[1+i]+list[i+2]
-    l=len(strr)-1
+list= [""] * 12 #creating a list to store 12 strings
+for i in range (12):#looping to input 
+    str1=input("enter name:")
+    list[i]=str1#assiging the string to the list
+i=0#re-initialising the varibale i to reuse it on another loop
+for i in range (10):#looping to concanate
+    strr=list[i]+list[1+i]+list[i+2]#joining the stings
+    l=len(strr)-1#cheaking for the length of the string
     str2=""
     while (l>=0):
        str2+=strr[l]
        l=l-1
     if(strr==str2):
-       print "palindrome"
+       print("palindrome")
     else:
-       print "not palindrome"
+       print("not palindrome")
